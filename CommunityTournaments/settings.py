7e0206a22515d62e3943238cb8a59b9b788b7716
marcc <marc.cerretti@gmail.com>
2019-05-04 12:29:35 +0100

    Attempting to get channels working 1

--- CommunityTournaments/settings.py
+++ CommunityTournaments/settings.py
@@ -151,7 +151,10 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [
+                ('127.0.0.1', 6379),
+                ('www.mcerretti.com', 6379)
+            ],
         },
     },
 }
--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn CommunityTournaments.wsgi:application --log-file -
+worker: python manage.py runworker -v2
