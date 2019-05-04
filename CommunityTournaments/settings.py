8bd783d06de4d8b4bac4dadf2be4a94804cd3311
marcc <marc.cerretti@gmail.com>
2019-05-04 21:37:09 +0100

    Attempting to get channels working 6  - changed host to REDIS_URL

--- CommunityTournaments/settings.py
+++ CommunityTournaments/settings.py
@@ -151,10 +151,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [
-                ('127.0.0.1', 6379),
-                ('www.mcerretti.com', 6379)
-            ],
+            "hosts": [os.environ['REDIS_URL']],
         },
     },
 }
